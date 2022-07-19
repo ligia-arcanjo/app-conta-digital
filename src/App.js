254963232ef4307f598ac76deb6acef6163be165
ligia <ligiaarcanjo84@gmail.com>
2022-07-19 20:57:33 -0300

    feat: criada as páginas e rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import StockList from './pages/StockList';
+import Trade from './pages/Trade';
+import Account from './pages/Account';
+
 function App() {
   return (
-    <div>
-      Desafio XP Inc
-    </div>
+    <Routes>
+      <Route path="/*" element={<Login />} />
+      <Route path="/acoes" element={<StockList />} />
+      <Route path="/compra-e-venda" element={<Trade />} />
+      <Route path="/conta" element={<Account />} />
+    </Routes>
   );
 }
 
