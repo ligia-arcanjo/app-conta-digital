91d64041dbe74afbb40931f5d5a156549cf98313
ligia <ligiaarcanjo84@gmail.com>
2022-07-21 23:17:12 -0300

    feat: atualizado StockList

--- src/pages/StockList.js
+++ src/pages/StockList.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Stocks from '../components/Stocks';
+import UserStocks from '../components/UserStocks';
 
 function StockList() {
+  const navigate = useNavigate();
+
+  const accessAccount = () => navigate('/conta');
+
   return (
     <>
-      <span>User</span>
+      <Header />
+      <UserStocks />
+      <Stocks />
 
-      <div id="minhas-acoes">
-        <h3>Minhas ações</h3>
-        <table>
-          <tr>
-            <th>Ação</th>
-            <th>Quantidade</th>
-            <th>Valor</th>
-            <th>Negociar</th>
-          </tr>
-          <tr>
-            <th>AZUL4</th>
-            <th>100</th>
-            <th>350,00</th>
-            <th>
-              <Link to="/compra-e-venda">Comprar</Link>
-              <Link to="/compra-e-venda">Vender</Link>
-            </th>
-          </tr>
-          <tr>
-            <th>PETR4</th>
-            <th>100</th>
-            <th>350,00</th>
-            <th>
-              <Link to="/compra-e-venda">Comprar</Link>
-              <Link to="/compra-e-venda">Vender</Link>
-            </th>
-          </tr>
-          <tr>
-            <th>VALE4</th>
-            <th>100</th>
-            <th>350,00</th>
-            <th>
-              <Link to="/compra-e-venda">Comprar</Link>
-              <Link to="/compra-e-venda">Vender</Link>
-            </th>
-          </tr>
-        </table>
-      </div>
-
-      <button type="button">Acessar conta</button>
+      <button onClick={accessAccount} type="button">Acessar conta</button>
     </>
   );
 }
