82141fdf4202ecc1ebac43ecbafddc6743b0b767
ligia <ligiaarcanjo84@gmail.com>
2022-07-21 23:16:13 -0300

    feat: criados os componentes UserStocks e Stocks

---
+++ src/components/Stocks.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAllStocks } from '../services/fetchStocks';
+
+function Stocks() {
+  const [stockList, setStockList] = useState();
+
+  useEffect(() => {
+    const getStockList = async () => {
+      const stocks = await getAllStocks();
+      setStockList(stocks);
+    };
+
+    getStockList();
+  }, []);
+
+  return (
+    <>
+      <h3>Ações disponíveis para compra</h3>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Ação</th>
+            <th>Quantidade</th>
+            <th>Valor</th>
+            <th>Negociar</th>
+          </tr>
+
+          {
+            stockList ? stockList.map((stock) => (
+              <tr key={stock.id}>
+                <th>{stock.empresa}</th>
+                <th>{stock.quantidade}</th>
+                <th>{`R$ ${stock.valor}`}</th>
+                <th>
+                  <Link to="/compra-e-venda">Comprar</Link>
+                </th>
+              </tr>
+            )) : null
+          }
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Stocks;
---
+++ src/components/UserStocks.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getUserStocks } from '../services/fetchStocks';
+
+function UserStocks() {
+  const [userStockList, setUserStockList] = useState();
+
+  useEffect(() => {
+    const getStockList = async () => {
+      const stocks = await getUserStocks();
+      setUserStockList(stocks);
+    };
+
+    getStockList();
+  }, []);
+
+  return (
+    <>
+      <h3>Minhas ações</h3>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Ação</th>
+            <th>Quantidade</th>
+            <th>Valor</th>
+            <th>Negociar</th>
+          </tr>
+
+          {
+            userStockList ? userStockList.map((stock) => (
+              <tr key={stock.id}>
+                <th>{stock.empresa}</th>
+                <th>{stock.quantidade}</th>
+                <th>{`R$ ${stock.valor}`}</th>
+                <th>
+                  <Link to="/compra-e-venda">Comprar</Link>
+                  <Link to="/compra-e-venda">Vender</Link>
+                </th>
+              </tr>
+            )) : null
+          }
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default UserStocks;
